c3997aed3e906c745dbe7d4bdee53a626134106c
Travis Shirk <travis@pobox.com>
2015-11-26 13:39:26 -0700

    @alias, error stuff, prompt fuixes, etc.

--- virtualenvwrapper_xonsh.py
+++ virtualenvwrapper_xonsh.py
@@ -5,13 +5,16 @@ import shutil
 import builtins
 import subprocess
 from pathlib import Path
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from os.path import expandvars, expanduser
 from builtins import __xonsh_env__ as env
-from xonsh.tools import TERM_COLORS
+from xonsh.tools import TERM_COLORS, get_app_color
 
 
 DEFAULT_VENV_DIR = "~/.virtualenvs"
+
+ENV_REQ_MSG = """A virtualenv is not appear to be active.
+See `workon --help` and/or `mkvirtualenv --help` for more details."""
 
 _BASIC_XONSH = """#!/usr/bin/env xonsh
 from builtins import __xonsh_env__ as env
@@ -31,6 +34,7 @@ predeactivate_script = Path('{predeactivate}')
 postdeactivate_script = Path('{postdeactivate}')
 
 $VIRTUAL_ENV = str(env_d)
+$VIRTUAL_ENV_PROMPT = '{prompt}'
 
 $PATH.insert(0, str(env_d / 'bin'))
 
@@ -54,7 +58,9 @@ def _deactivate(args, stdin=None):
     except Exception as ex:
         print("%s: %s" % (postdeactivate_script.parts[-1], ex))
 
-    del env["VIRTUAL_ENV"]
+    del env['VIRTUAL_ENV']
+    if 'VIRTUAL_ENV_PROMPT' in env:
+        del env['VIRTUAL_ENV_PROMPT']
     del builtins.aliases['deactivate']
 
 builtins.aliases['deactivate'] = _deactivate
@@ -85,6 +91,34 @@ _hook_scripts = {s: ("{}.xonsh".format(s), "{}_XONSH".format(s.upper()))
 class Error(RuntimeError):
     pass
 
+def _success(stdout=None, stderr=None):
+    return (stdout, stderr, 0)
+
+def _error(sterr=None, returncode=1):
+    return (None, stderr, returncode)
+
+### Decorators ###
+
+def alias(func):
+    builtins.aliases[func.__name__] = func
+    def wrapper(args, stdin=None):
+        try:
+            return func(args, stdin=stdin)
+        except Error as err:
+            return _error(str(err))
+    return func
+
+
+def ensureEnv(func):
+    '''Decorator for ensuring a VIRTUAL_ENV is set.'''
+    def wrap(*args, **kwargs):
+        if envDir() is None:
+            raise Error(ENV_REQ_MSG)
+        else:
+            return func(*args, **kwargs)
+    return wrap
+
+### Hook functions ###
 
 def _concreteHookPath(hook, hook_d):
     return Path(hook_d) / "bin" / Path(_hook_scripts[hook][0])
@@ -114,16 +148,6 @@ def runHookScript(script, env_d=None):
             print("{} error:\n{}".format(hook_script, ex), file=sys.stderr)
 
 
-def ensureEnv(func):
-    '''Decorator for ensuring a VIRTUAL_ENV is set.'''
-    def wrap(*args, **kwargs):
-        if envDir() is None:
-            raise Error(ENV_REQ_MSG)
-        else:
-            return func(*args, **kwargs)
-    return wrap
-
-
 def envDir():
     '''Return the virtualenv root directory or None if not set.'''
     if ("VIRTUAL_ENV" not in env) or not env["VIRTUAL_ENV"]:
@@ -133,7 +157,8 @@ def envDir():
 
 
 def envRootDir():
-    '''The directory where virtualenv are stored/created.'''
+    '''The directory where virtualenv are stored/created. If the root cannot be
+    determined and Error exception is raise.'''
     for var in ['VIRTUALENVWRAPPER_XONSH_DIR', 'VIRTUALENVWRAPPER_HOOK_DIR']:
         if var in env:
             root_dir = Path(expandvars(expanduser(env[var])))
@@ -169,16 +194,14 @@ def projectDir():
 
 
 def getAllEnvs():
-    '''Returns a list of all virtualenv directories.'''
+    '''Returns a list of all virtualenv directories. May raise an Error
+    exception if the virtualenvwrapper root direction cannot be determined.'''
     all_envs = []
     for f in envRootDir().iterdir():
         if f.is_dir():
             all_envs.append(f.name)
     return all_envs
 
-
-ENV_REQ_MSG = """A virtualenv is not appear to be active.
-See `workon --help` and/or `mkvirtualenv --help` for more details."""
 
 def useEnv(env_name):
     if env_name is None:
@@ -194,13 +217,15 @@ def useEnv(env_name):
     return env_d
 
 
-def activate(env_d, reinstall=False):
+def activate(env_d, reinstall=False, prompt=None):
     env_d = Path(useEnv(env_d))
+    env_name = env_d.parts[-1]
 
     subst = {"env_d": str(env_d),
              "predeactivate": str(_concreteHookPath("predeactivate", env_d)),
              "postdeactivate": str(_concreteHookPath("postdeactivate", env_d)),
-             "env_name": env_d.parts[-1],
+             "env_name": env_name,
+             "prompt": prompt or env_name,
             }
     for hook in _hook_scripts:
         script = _concreteHookPath(hook, env_d)
@@ -215,10 +240,15 @@ def activate(env_d, reinstall=False):
 
     runHookScript("preactivate")
     runHookScript("activate")
+    if prompt and env["VIRTUAL_ENV_PROMPT"] != prompt:
+        # Use wants at prompt different than what is stashed in activate
+        env["VIRTUAL_ENV_PROMPT"] = prompt
     runHookScript("postactivate")
 
+### Aliases ###
 
 @ensureEnv
+@alias
 def cdproject(args=None, stdin=None):
     parser = ArgumentParser(prog="showvirtualenv")
     parser.add_argument("--set", dest="set_dir", default=None, type=Path,
@@ -250,17 +280,20 @@ def cdproject(args=None, stdin=None):
 
 
 @ensureEnv
+@alias
 def cdvirtualenv(args, stdin=None):
     builtins.aliases["cd"]([str(envDir())])
 
 
 @ensureEnv
+@alias
 def cdsitepackages(args, stdin=None):
     env_d = envDir() / "lib"
     pkgs_dir = [f for f in env_d.glob("python*")][0] / 'site-packages'
     builtins.aliases["cd"]([str(pkgs_dir)])
 
 
+@alias
 def lsvirtualenv(args, stdin=None):
     parser = ArgumentParser(prog="lsvirtualenv")
     group = parser.add_mutually_exclusive_group()
@@ -279,6 +312,7 @@ def lsvirtualenv(args, stdin=None):
             print()
 
 
+@alias
 def cpvirtualenv(args, stdin=None):
     parser = ArgumentParser(prog="cpvirtualenv")
     parser.add_argument("src_env")
@@ -298,13 +332,21 @@ def cpvirtualenv(args, stdin=None):
     print("Copying {} as {}...".format(src_env.parts[-1], dst_env.parts[-1]))
     shutil.copytree(str(src_env), str(dst_env), symlinks=True)
 
-    # TODO: fix .project, shows old name
-    # TODO: fix active, shows old old VIRTUAL_ENV
+    # Fix up the new activate script
+    with (src_env / "bin" / "activate.xonsh").open("r") as fp:
+        curr_activate = fp.read()
+    with (dst_env / "bin" / "activate.xonsh").open("w") as fp:
+        for line in curr_activate.splitlines():
+            line = line.replace(str(src_env), str(dst_env))
+            if line.startswith("$VIRTUAL_ENV_PROMPT = "):
+                line = "$VIRTUAL_ENV_PROMPT = '{}'".format(dst_env.parts[-1])
+            fp.write(line + '\n')
 
     runHookScript("postmkvirtualenv", src_env)
     runHookScript("postcpvirtualenv", src_env)
 
 
+@alias
 def rmvirtualenv(args, stdin=None):
     parser = ArgumentParser(prog="rmvirtualenv")
     parser.add_argument("env_names", nargs='+',
@@ -326,6 +368,7 @@ def rmvirtualenv(args, stdin=None):
         runHookScript("postrmvirtualenv", env_d)
 
 
+@alias
 def showvirtualenv(args, stdin=None):
     parser = ArgumentParser(prog="showvirtualenv")
     parser.add_argument("env_name", nargs='?', default=None,
@@ -337,6 +380,7 @@ def showvirtualenv(args, stdin=None):
     runHookScript("get_env_details", env_d)
 
 
+@alias
 def mkvirtualenv(args, stdin=None):
     proc = subprocess.Popen(["virtualenv", "--help"], universal_newlines=True,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -345,20 +389,30 @@ def mkvirtualenv(args, stdin=None):
     if proc.returncode != 0:
         raise Error("Unable to run virtualenv: " + stderr)
 
-    # FIXME: ArgumentParser is formatted epilog, want it untouched
-    parser = ArgumentParser(prog="mkvirtualenv", epilog=stdout)
+    parser = ArgumentParser(prog="mkvirtualenv", epilog=stdout,
+                            formatter_class=RawDescriptionHelpFormatter)
     if len(args) > 1:
         # Move the env_name postiion for argparse finds it first rather than
         # put in the virtualenv opts.
         args = [args[-1]] + args[:-1]
     parser.add_argument("env_name", nargs=1)
-    parser.add_argument("-a", action="store", dest="project_path")
-    parser.add_argument("-i", action="append", dest="install_pkgs", default=[])
-    parser.add_argument("-r", action="append", dest="req_files", default=[])
+    parser.add_argument("-a", action="store", dest="project_path",
+                        help="The full path to the project directory.")
+    parser.add_argument("-i", action="append", dest="install_pkgs", default=[],
+                        help="Install a package after the environment is "
+                             "created. This option may be repeated.")
+    parser.add_argument("-r", action="append", dest="req_files", default=[],
+                        help="Provide a pip requirements file to install a "
+                             "base set of packages into the new environment.")
+    parser.add_argument("--prompt", dest="prompt", default=None,
+                        help="Provide an alternate prompt for the environment.")
 
     args, venv_args  = parser.parse_known_args(args)
+    if args.prompt:
+        venv_args += ["--prompt={}".format(args.prompt)]
 
     env_d = envRootDir() / args.env_name[0]
+    env_name = env_d.parts[-1]
     if env_d.exists():
         return None, "virtualenv exists: {}".format(env_d)
 
@@ -367,7 +421,8 @@ def mkvirtualenv(args, stdin=None):
     if result != 0:
         return 1
 
-    workon([env_d.parts[-1]])
+    prompt = args.prompt or env_name
+    workon([env_name, "--prompt={}".format(args.prompt)])
 
     if args.project_path:
         cdproject(["--set", args.project_path])
@@ -382,6 +437,7 @@ def mkvirtualenv(args, stdin=None):
                          env=popen_env).wait()
 
 
+@alias
 def workon(args, stdin=None):
     desc = "Deactivate any currently activated virtualenv and activate the "\
            "named environment, triggering any hooks in the process."
@@ -391,6 +447,8 @@ def workon(args, stdin=None):
     parser = ArgumentParser('workon', description=desc)
     parser.add_argument("--reinstall", action="store_true",
                         help="Reinstall hook files.")
+    parser.add_argument("--prompt", dest="prompt", default=None,
+                        help="Provide an alternate prompt for the environment.")
     parser.add_argument("env_name", nargs='?', default=None,
                         help="If not specified a list of available "
                              "environments is printed.")
@@ -405,32 +463,31 @@ def workon(args, stdin=None):
         runHookScript("deactivate")
 
     env_d = useEnv(args.env_name)
-    activate(env_d, reinstall=args.reinstall)
+    activate(env_d, reinstall=args.reinstall, prompt=args.prompt)
     cdproject(['-q'])
 
-
-builtins.aliases["workon"] = workon
-builtins.aliases["cdproject"] = cdproject
-builtins.aliases["cdvirtualenv"] = cdvirtualenv
-builtins.aliases["lsvirtualenv"] = lsvirtualenv
-builtins.aliases["rmvirtualenv"] = rmvirtualenv
-builtins.aliases["cpvirtualenv"] = cpvirtualenv
-builtins.aliases["mkvirtualenv"] = mkvirtualenv
-builtins.aliases["cdsitepackages"] = cdsitepackages
-builtins.aliases["showvirtualenv"] = showvirtualenv
 
 builtins.projectDir = projectDir
 builtins.envDir = envDir
 
+### Prompts ###
+
 def _promptVirtualenv():
-    # FIXME: Allow color customization
+    p = None
     if "VIRTUAL_ENV" in env and env["VIRTUAL_ENV"]:
-        return "(" + TERM_COLORS["YELLOW"] \
-                + os.path.basename(__xonsh_env__["VIRTUAL_ENV"]) \
-                + TERM_COLORS["NO_COLOR"] \
-                + ")"
+        env_name = os.path.basename(env["VIRTUAL_ENV"])
+        if "VIRTUAL_ENV_PROMPT" in env and env["VIRTUAL_ENV_PROMPT"]:
+            p = env["VIRTUAL_ENV_PROMPT"]
+        else:
+            p = env_name
+
+    if p:
+        c = get_app_color("virtualenvwrapper_xonsh", "prompt")
+        n = TERM_COLORS["NO_COLOR"] if c else ""
+        return "({c}{p}{n})".format(**locals())
     else:
         return ""
+
 env['FORMATTER_DICT']['virtualenv'] = _promptVirtualenv
 
 def updatePrompt():
